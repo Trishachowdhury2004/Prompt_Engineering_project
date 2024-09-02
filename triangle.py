30084a6a9eda299de94400643fde67670a975ba3
Trishachowdhury2004 <trishachowdhury209@gmail.com>
2024-09-02 16:46:03 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Turtle Test")
+
+
+t = turtle.Turtle()
+t.speed(1)  
+
+for _ in range(3):
+    t.forward(100) 
+    t.left(120)   
+
+  
+turtle.done()
+
